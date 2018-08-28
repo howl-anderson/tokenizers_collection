950e21cd1164f6933eb21b64d0f07ec5af598d6a
Xiaoquan Kong <u1mail2me@gmail.com>
2018-08-29 00:44:32 +0800

    Bump version: 0.1.0 → 0.1.1

--- tokenizers_collection/__init__.py
+++ tokenizers_collection/__init__.py
@@ -5,7 +5,7 @@ import os
 
 __author__ = """Xiaoquan Kong"""
 __email__ = 'u1mail2me@gmail.com'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 ltp_data_base_dir = os.path.join(os.environ.get("HOME"), '.ltp_data')
 
